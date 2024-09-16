e8670fd9553d1a773f389347f8aa83a9227ca795
joseph mwalai <josephmwalai@gmail.com>
2024-09-16 15:56:17 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+
+    if (menu && icon) {
+        menu.classList.toggle("open");  
+        icon.classList.toggle("open");  
+    } else {
+        console.error("Menu or Icon not found");
+    }
+}
